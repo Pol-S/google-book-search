3792d39e089bbe18848f83dc965faa5ed52014eb
Pol S <plsbnjy@gmail.com>
2019-02-14 22:51:07 -0600

    Add files via upload
    Attempting to link an external javascript file to index.ejs. Wish me luck.

---
+++ views/pages/interface.js
@@ -0,0 +1,44 @@
+  
+        function checkQuery(booksearch) {        
+          if (booksearch.trim() === '' ) {
+            alert("No character inputted. Please input a title/author/publishing house.");
+          }
+          else {
+            booksearch = booksearch.replace(/\s/g, '+');
+            callApi(booksearch)
+            buttonSwap()
+          }            
+        }
+  
+        function callApi(booksearch) {
+          fetch('https://www.googleapis.com/books/v1/volumes?q=' + booksearch)
+            .then(function(response) {
+              return response.json();
+            })
+            .then(function(myJson) {
+              console.log(JSON.stringify(myJson));
+              bookInfo(myJson);
+            })
+            .catch(error => {
+              console.log('Connection Error');
+              printError();
+            });
+        }
+  
+        function bookInfo(response) {
+        for (var i = 0; i < response.items.length; i++) {
+          var item = response.items[i];
+          var book_hash = {}
+          book_hash['info'] = item.volumeInfo.infoLink
+          book_hash['title'] = item.volumeInfo.title
+          book_hash['authors'] = item.volumeInfo.authors
+          book_hash['publisher'] = item.volumeInfo.publisher
+          book_hash['cover'] = item.volumeInfo.imageLinks.thumbnail
+          printResults(book_hash)
+        }
+      }
+
+      function sum(a, b) {
+        return a + b;
+      }
+      module.exports = sum
