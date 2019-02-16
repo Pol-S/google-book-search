c1c6f1376b82feb2a33a3efe94c330967f1f2210
Pol S <plsbnjy@gmail.com>
2019-02-15 19:09:06 -0600

    Add files via upload

--- test.js
+++ test.js
@@ -2,25 +2,17 @@
 const checkQuery = require('./views/pages/interface');
 const callApi = require('./views/pages/interface');
 const bookInfo = require('./views/pages/interface');
+const buttonSwap = require('./views/pages/interface');
 
 
-test('that a query of all blank spaces throws an alert', () => {
+test('A query of all blank spaces throws an alert', () => {
   expect(checkQuery(' ')).toBeTruthy();
 });
 
-describe('checkQuery', () => {
-  test('query is formatted correctly, a call will be made', () => {
-    const callApi = jest.fn();
-    checkQuery(callApi, 'test');
-    expect(callApi).toHaveBeenCalled();  
-  });
-
-  test('query is formatted incorrectly, a call will not be made', () => {
-    const callApi = jest.fn();
-    checkQuery(callApi, ' ');
-    expect(callApi).not.toHaveBeenCalled();  
-  });
+test('If query is formatted correctly, a call will be made', () => {
+  expect(checkQuery('test')).toBeTruthy();
 });
+
 
 
 
